39680c2a11acb642b4ea64897e25eceb4b1fd421
Matias Nicoletti <matiasnicolettmdp@gmail.com>
2020-08-15 00:10:23 -0300

    Edit form

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,18 +1,35 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 import { Post } from '../post.model';
 import { NgForm } from '@angular/forms';
 import { PostsService } from '../posts.service';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 @Component({
     selector: 'app-post-create',
     templateUrl: './post-create.component.html',
     styleUrls: ['./post-create.component.css']
 })
-export class PostCreateComponent {
+export class PostCreateComponent implements OnInit{
     enteredContent = '';
     enteredTitle = '';
+    private mode = 'create';
+    private postId:string;
+    private post:Post;
     // @Output() postCreated = new EventEmitter<Post>();
     // Output so the event is able to be listened on others compoentents
-    constructor(private postsService: PostsService) { }
+    constructor(private postsService: PostsService, private route: ActivatedRoute) { }
+
+    ngOnInit(){
+        this.route.paramMap.subscribe((paramMap: ParamMap)=>{
+            if(paramMap.has('id')){
+                this.mode = 'edit';
+                this.postId = paramMap.get('id');
+                this.post = this.postsService.getPost(this.postId);
+            }else{
+                this.mode = 'create';
+                this.postId = null;
+            }
+        });
+    }
 
     onAddPost(form: NgForm) {
         if (form.invalid) {
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -29,6 +29,10 @@ export class PostsService {
     return this.postUpdated.asObservable();
   }
 
+  getPost(id:string){
+    return{...this.posts.find(p => p.id===id)};
+  }
+
   addPost(title: string, content: string) {
     const post: Post = { id: null, title, content };
     this.http.post<{ message: string, postId:string }>('http://localhost:3000/api/posts', post)
