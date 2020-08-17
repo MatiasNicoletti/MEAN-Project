81ac6719f20f4b94aacb6b543ba8f844f0b2561f
Matias Nicoletti <matiasnicolettmdp@gmail.com>
2020-08-17 12:21:02 -0300

    Show or hide links if user is auth

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,12 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { AuthData } from './auth-data.model';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
   })
 export class AuthService{
     private token:string;
+    private authStatusListener = new Subject<boolean>();
     constructor(private http: HttpClient){}
 
     createUser(email: string, password:string){
@@ -22,7 +24,12 @@ export class AuthService{
         this.http.post<{token:string}>('http://localhost:3000/api/users/login', authData)
             .subscribe(response => {
                 this.token = response.token;
+                this.authStatusListener.next(true);
             })
+    }
+
+    getAuthStatusListener(){
+        return this.authStatusListener.asObservable();
     }
 
     getToken(){
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,27 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.css']
 })
-export class HeaderComponent{
+export class HeaderComponent implements OnInit, OnDestroy{
+    private authListenerSubscription: Subscription;
+    userIsAuthenticated:boolean = false;
+    constructor(private authService: AuthService){
+
+    }
+
+    ngOnInit(){
+        this.authListenerSubscription = this.authService.getAuthStatusListener().subscribe(isAuthenticated => {
+            this.userIsAuthenticated = isAuthenticated;
+        });
+    }
+
+    ngOnDestroy(){
+        this.authListenerSubscription.unsubscribe();
+    }
     
 }
